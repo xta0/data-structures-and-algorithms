031cb449e8c6d33072d031b4a81844c178375097
xta0 <jayson.xu@foxmail.com>
2018-01-19 08:38:31 -0500

    update

--- Pre-C/week7/hw1.cpp
+++ Pre-C/week7/hw1.cpp
@@ -1,37 +1,30 @@
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
-int main()
-{
-
-    int n = 0, x = 0;
-    cin >> n;
-    double a_0118 = 0, a_1935 = 0, a_3660 = 0, a_61 = 0;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> x;
-        if (x > 0 && x <= 18)
-        {
-            a_0118++;
-        }
-        else if (x > 18 && x <= 35)
-        {
-            a_1935++;
-        }
-        else if (x > 36 && x <= 60)
-        {
-            a_3660++;
-        }
-        else
-        {
-            a_61++;
-        }
-    }
-    cout << "1-18: " << fixed << setprecision(2) << a_0118 / n * 100 << "%"<<"\n";
-    cout << "19-35: " << fixed << setprecision(2) << a_1935 / n * 100 << "%"<<"\n";
-    cout << "36-60: " << fixed << setprecision(2) << a_3660 / n * 100 << "%"<<"\n";
-    cout << "60-: " << fixed << setprecision(2) << a_61 / n * 100 << "%"<<"\n";
-
-    return 0;
-}
+#include<iostream>  
+#include <iomanip>  
+using namespace std;  
+int main() {  
+int patient=-1;  
+int n;  
+cin >> n;  
+double a = 0;  
+double b = 0;  
+double c = 0;  
+double d = 0;  
+for (int i = 0; i<n; i++) {  
+cin >> patient;  
+if (patient <= 18)  
+a++;  
+  
+  
+if (patient <= 35 && patient >= 19)  
+b++;  
+if (patient <= 60 && patient >= 36)  
+c++;  
+if (patient>60)  
+d++;  
+}  
+cout << "1-18: " << fixed << setprecision(2) << a / n * 100 << "%" << endl;  
+cout << "19-35: " << fixed << setprecision(2) << b / n * 100 << "%" << endl;  
+cout << "36-60: " << fixed << setprecision(2) <<c / n * 100 << "%" << endl;  
+cout << "60-: " << fixed << setprecision(2) << d / n * 100 << "%" << endl;  
+return 0;  
+}  
