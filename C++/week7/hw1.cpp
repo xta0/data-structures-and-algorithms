8508e3779bdb98b5731b9a973022e90980ece24c
xta0 <jayson.xu@foxmail.com>
2018-02-09 19:35:31 -0500

    update

--- C++/week7/hw1.cpp
+++ C++/week7/hw1.cpp
@@ -1,68 +1,97 @@
 #include <iostream>
 using namespace std;
 // 在此处补充你的代码
-#include <vector>
 
 template <class T>
-class CArray2D{
-    vector<vector<T>> arr2d={0,{0,0}};
+class CArray{
+    T* data=NULL;
+    int n=0;
 public:
-    CArray2D (
-              unsigned int row,
-              unsigned int column){
-        arr2d = {row,{static_cast<int>(column)}};
-    };
-    vector<T>& operator[](int index){
-        return arr2d[index];
+    CArray():CArray(0){}
+    CArray(int n){
+        // cout<<"Constructor-->"<<__FUNCTION__<<":"<<this<<endl;
+    }
+    ~CArray(){
+        // cout<<"Destructor-->"<<__FUNCTION__<<":"<<this<<endl;
+        if(data){
+            delete data;
+            data = NULL;
+        }
+    }
+    void set(int _n){
+        if(data){
+            delete data;
+            data = NULL;
+        }
+        n = _n;
+        data = new T[_n];
+    }
+    T& operator[](int index){
+        return data[index];
+    }
+};
+template <class T>
+class CArray2D{
+    CArray<T>* data=NULL;
+    int col=0;
+    int row=0;
+public:
+    CArray2D():CArray2D(0,0){}
+    CArray2D (int _row,int _col):col(_col),row(_row){};
+    ~CArray2D(){
+        if(data){
+            delete [] data;data = NULL;
+        }
+    }
+    void set(int _row, int _col){
+        row=_row;
+        col=_col;
+        data = new CArray<T>[row];
+        for(int i=0;i<_row; ++i){
+            data[i].set(_col);
+        }
+    }
+    CArray<T>& operator[](int index){
+        return data[index];
     }
 };
 
 template <class T>
 class CArray3D{
-    
-private:
-    // vector<CArray2D<T>> arr3d={0,{0,0}};
-    vector<vector<vector<T>>> arr3d;
-    
+    CArray2D<T>* data=NULL;
+    int col=0;
+    int row=0;
+    int z = 0;
 public:
-    CArray3D(
-             unsigned int zindex,
-             unsigned int row,
-             unsigned int column){
-        // arr3d = {zindex,{row,column}};
-        arr3d = {zindex,{row,{static_cast<int>(column)}}};
+    CArray3D():CArray3D(0,0,0){}
+    CArray3D (int _z , int _row,int _col):col(_col),row(_row),z(_z){
+        data = new CArray2D<T>[z];
+        for(int i=0; i<z; ++i){
+            data[i].set(_row, _col);
+        }
+    };
+    ~CArray3D(){
+        if(data){
+            delete [] data;data = NULL;
+        }
     }
-    // CArray2D<T>& operator[](int index){
-    //     return arr3d[index];
-    // }
-    vector<vector<T>>& operator[](int index){
-        return arr3d[index];
+    CArray2D<T>& operator[](int index){
+        return data[index];
     }
 };
 int main()
 {
-    // CArray3D<int> a(3,4,5);
-    // int No = 0;
-    // for( int i = 0; i < 3; ++ i )
-    //     for( int j = 0; j < 4; ++j )
-    //         for( int k = 0; k < 5; ++k )
-    //             a[i][j][k] = No ++;
-    // for( int i = 0; i < 3; ++ i )
-    //     for( int j = 0; j < 4; ++j )
-    //         for( int k = 0; k < 5; ++k )
-    //             cout << a[i][j][k] << ",";
+     CArray3D<int> a(3,4,5);
+     int No = 0;
+     for( int i = 0; i < 3; ++ i )
+         for( int j = 0; j < 4; ++j )
+             for( int k = 0; k < 5; ++k )
+                 a[i][j][k] = No ++;
+     for( int i = 0; i < 3; ++ i )
+         for( int j = 0; j < 4; ++j )
+             for( int k = 0; k < 5; ++k )
+                 cout << a[i][j][k] << ",";
     
-    int No=0;
-    CArray2D<int> b(4,5);
-    for(int i=0;i<4;i++){
-        for(int j=0;j<5;j++){
-            b[i][j] =No++;
-        }
-    }
-    for(int i=0;i<4;i++){
-        for(int j=0;j<5;j++){
-            cout<<b[i][j]<<",";
-        }
-    }
     return 0;
 }
+
