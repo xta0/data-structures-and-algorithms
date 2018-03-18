78a0a8b7ea0176d97a7b7089d7c0e10308707cf5
xta0 <jayson.xu@foxmail.com>
2018-03-17 23:08:06 -0400

    update

--- algo/week3/hw2.cpp
+++ algo/week3/hw2.cpp
@@ -1,69 +1,129 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <vector>
-#include <stack>
+
 using namespace std;
 
 class Node{
 public:
     string name;
+    int layer;
     vector<Node* > children;
-    ~Node(){
-        cout<<__func__<<": "<<name<<endl;
-    }
+    Node(string name, int layer):name(name),layer(layer){}
 };
 
-void clean(Node* root){
-    root -> name = "";
+static int layer = 0;
+static int round = 1;
+static string token = "";
+
+void clean_state(){
+    layer = 0;
+    round ++;
+    token = "";
+}
+
+void clean_tree(Node* root){
+    for(int i=0; i<root->children.size();++i){
+        Node* node = root->children[i];
+        if(node -> children.size() > 0){
+            clean_tree(node);
+        }
+        if(node){
+            delete node;
+            node = NULL;
+        }
+    }
     root -> children.clear();
 }
 
-void print(Node* root){
-    cout<<root->name<<endl;
-    if(root->children.size() > 0){
-        for(auto node : root->children){
-            if(node -> children.size() > 0){
-                cout<<"|     "<<node->name<<endl;
-                print(node);
-            }else{
-                cout<<node->name<<endl;
-            }
-        }
+void print_node(Node* node){
+    for(int i=0;i<node->layer;++i){
+        cout<<"|     ";
     }
+    cout<<node->name<<endl;
 }
-void fill(Node* root){
-    string tmp;
-    cin >> tmp;
-    if (tmp != "]"){
-        char x = tmp[0];
-        if(x == 'f' || x == 'd'){
-            Node* n = new Node{tmp,{}};
-            if(x == 'd'){
-                fill(n);
-            }
-            root->children.push_back(n);
+
+void print_tree(Node* root){
+    print_node(root);
+    for(int i=0; i<root->children.size();++i){
+        Node* node = root->children[i];
+        if(node -> children.size() > 0){
+            print_tree(node);
+        }else{
+            print_node(node);
         }
     }
 }
 
-void tree(Node& root){
-    string tmp;
-    int count = 1;
-    while(tmp != "#"){
-        cin >> tmp;
-        if(tmp == "*"){
-            print(&root);
-        }else{
-            cout<<"DATA SET "<<count<<":"<<endl;
-            fill(&root);
-            clean(&root);
-            count ++;
+bool comp1(Node* n1, Node* n2){
+    char c1 = n1->name[0];
+    char c2 = n2->name[0];
+    if(c1 == 'd' && c2 == 'd'){
+        return false;
+    }else if(c1 == 'f' && c2 == 'd'){
+        return false;
+    }else if(c1 == 'd' && c2 == 'f'){
+        return true;
+    }else{
+        return n1->name<n2->name;
+    }
+}
+void sort_tree(Node* root){
+    for(int i=0; i<root->children.size();++i){
+        Node* node = root->children[i];
+        if(node -> children.size() > 0){
+            sort_tree(node);
         }
+    }
+    sort(root->children.begin(), root->children.end(), comp1);
+}
+void print(Node* root){
+    sort_tree(root);
+    cout<<"DATA SET "<<round<<":"<<endl;
+    print_tree(root);
+    cout<<endl;
+}
+
+void tree(Node* root){
+
+    while(token != "#"){
+        if(token == "*"){
+            if(root->layer > 0){
+                return;
+            }
+            print(root);
+            clean_tree(root);
+            clean_state();
+        }else{
+            cin >> token;
+            cin.get();
+            if(token == "]"){
+                bool ret = layer > 0;
+                layer--;
+                if(ret){
+                    return;
+                }
+            }else{
+                char x = token[0];
+                if(x == 'f' || x == 'd'){
+                    Node* n = new Node(token,layer);
+                    if(x == 'd'){
+                        layer++;
+                        n -> layer = layer;
+                        tree(n);
+                    }
+                    root->children.push_back(n);
+                }
+            }
+        }
+     
     }
 }
 
 int main(){
-    Node root = {"ROOT",{}};
-    tree(root);
+    Node root("ROOT",0);
+    tree(&root);
     return 0;
 }
+
