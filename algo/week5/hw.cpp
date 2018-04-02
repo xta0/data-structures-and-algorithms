06e152e8c62b0d0fd2afbaf47e6761c981ead6e6
xta0 <jayson.xu@foxmail.com>
2018-04-02 13:53:52 -0400

    update

---
+++ algo/week5/hw.cpp
@@ -0,0 +1,21 @@
+#include<iostream>  
+#include <vector>
+using namespace std;  
+
+int main(){  
+    int N, M;
+    cin >> N >> M;
+    int wi, di;
+    vector<int> last(M+1, 0);
+    vector<int> next(M+1, 0);
+    for (int i=0; i<N; i++)
+    {
+        cin >> wi >> di;
+        copy(next.begin(), next.end(), last.begin());
+        for(int j=wi; j<=M; j++) {
+            next[j] = max(last[j], last[j-wi]+di);
+        }
+    }
+    cout << next[M] << endl;
+    return 0;  
+}  
