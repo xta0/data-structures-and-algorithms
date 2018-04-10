9e94bd9a01a0591a9d6eecbbe6d9fc9825d9cbe5
xta0 <jayson.xu@foxmail.com>
2018-04-10 17:38:17 -0400

    update

--- sort/header.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void print_array(const char *title, const int *list, int len)
-{
-    printf("%s\n", title);   
-    for (int i = 0; i < len; ++i){
-        printf("%d,", list[i]);
-    }
-}
