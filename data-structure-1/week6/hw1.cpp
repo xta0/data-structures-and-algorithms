69d4e5754a549ade3bf0171c651784b056f446a1
xta0 <jayson.xu@foxmail.com>
2018-05-05 00:33:17 -0400

    update

--- data-structure-1/week6/hw1.cpp
+++ data-structure-1/week6/hw1.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <string>
+#include <map>
 #include <vector>
 #include <queue>
 #include <stack>
 #include <algorithm>
 using namespace std;
 
-
-
 struct HuffmanTreeNode{
     HuffmanTreeNode* left = NULL;
     HuffmanTreeNode* right = NULL;
     HuffmanTreeNode* parent = NULL;
     int weight = 0;
-    bool isVisited = false;
-    
 };
 
 struct comp{
@@ -23,87 +20,55 @@ struct comp{
     }
 };
 
-struct HuffmanTree{
-    HuffmanTreeNode* root;
-    HuffmanTree(vector<int>& weight){
-        priority_queue<HuffmanTreeNode*,std::vector<HuffmanTreeNode* >,comp> heap;
-        size_t n = weight.size();
-        for(int i=0; i<n; ++i){
-            HuffmanTreeNode* node = new HuffmanTreeNode();
-            node->weight = weight[i];
-            node->left = node->right = node->parent = NULL;
-            heap.push(node);
-        }
-        for(int i=0; i<n-1; i++){ //两个节点合成一个，共有n-1次合并建立Huffman树
-            HuffmanTreeNode* first = heap.top();
-            heap.pop();
-            HuffmanTreeNode* second = heap.top();
-            heap.pop();
-            HuffmanTreeNode* parent = new HuffmanTreeNode();
-            first->parent = parent;
-            second->parent = parent;
-            parent->weight = first->weight + second->weight;
-            parent->left  = first;
-            parent->right = second;
-            heap.push(parent);
-            root = parent;
-        }
+HuffmanTreeNode* buildHuffmanTree(vector<int>& weight){
+    HuffmanTreeNode* root = NULL;
+    priority_queue<HuffmanTreeNode*,std::vector<HuffmanTreeNode* >,comp> heap;
+    size_t n = weight.size();
+    for(int i=0; i<n; ++i){
+        HuffmanTreeNode* node = new HuffmanTreeNode();
+        node->weight = weight[i];
+        node->left = node->right = node->parent = NULL;
+        heap.push(node);
+    }
+    for(int i=0; i<n-1; i++){ //两个节点合成一个，共有n-1次合并建立Huffman树
+        HuffmanTreeNode* first = heap.top();
+        heap.pop();
+        HuffmanTreeNode* second = heap.top();
+        heap.pop();
+        HuffmanTreeNode* parent = new HuffmanTreeNode();
+        first->parent = parent;
+        second->parent = parent;
+        parent->weight = first->weight + second->weight;
+        parent->left  = first;
+        parent->right = second;
+        heap.push(parent);
+        root = parent;
     }
     
-    HuffmanTreeNode* find_node(int weight){
-        HuffmanTreeNode* ret = NULL;
-        
-        //bfs
-        queue<HuffmanTreeNode* > qq;
-        qq.push(root);
-        while(!qq.empty()){
-            HuffmanTreeNode* front = qq.front();
-            qq.pop();
-            if(front -> weight == weight &&
-               front->left == NULL &&
-               front->right == NULL &&
-               front->isVisited == false){
-                
-                ret = front;
-                ret->isVisited = true;
-                break;
-            }
-            if(front->left){
-                qq.push(front->left);
-            }
-            if(front->right){
-                qq.push(front->right);
-            }
+    return root;
+}
+
+void traverse(HuffmanTreeNode* root, vector<int>& codes){
+    
+    if(root&&root->left==NULL&&root->right==NULL){
+        cout<<root->weight<<": ";
+        for(auto n:codes){
+            cout<<n;
         }
-        
-        
-        return ret;
+        cout<<endl;
     }
     
-    string code(int weight){
-        string ret("");
-        HuffmanTreeNode* node = find_node(weight);
-        stack<char> ss;
-        while(node){
-            HuffmanTreeNode* parent = node->parent;
-            if(!parent){
-                break;
-            }
-            if(node == parent->left){
-                ss.push('0');
-            }else{
-                ss.push('1');
-            }
-            node = node->parent;
-        }
-        while(!ss.empty()){
-            ret+=string(1,ss.top());
-            ss.pop();
-        }
-        
-        return ret;
+    if(root->left){
+        codes.push_back(0);
+        traverse(root->left,codes);
+        codes.pop_back();
     }
-        
+   
+    if(root->right){
+        codes.push_back(1);
+        traverse(root->right, codes);
+        codes.pop_back();
+    }
 };
 
 int main(){
@@ -116,13 +81,9 @@ int main(){
     }
     std::sort(vec.begin(), vec.end());
     
-    HuffmanTree tree = HuffmanTree(vec);
-    int sum = 0;
-    for(int i=0;i<vec.size();++i){
-        string code = tree.code(vec[i]);
-        sum += vec[i]*code.length();
-    }
-    cout<<sum<<endl;
+    HuffmanTreeNode* root = buildHuffmanTree(vec);
+    vector<int> codes;
+    traverse(root, codes);
     
     return 0;
 }
